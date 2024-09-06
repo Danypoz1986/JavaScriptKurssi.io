8e2019bd0bd5f0629d4a1704bce9a906326eec65
Daniel Pozzoli <daniel.pozzoli86@gmail.com>
2024-09-06 14:48:12 +0300

    Remove all files and folders except WorkShop1

--- workshop2/Exercise4/jskoodi4.js
+++
@@ -1,5 +0,0 @@
-let isStudent = confirm("Are you a student?")
-if (isStudent)
-    document.write("<h1 style='color: red'>Welcome student</h1>")
-else 
-    document.write("<h1 style='color: red'>Sorry, this page is for students only</h1>")
