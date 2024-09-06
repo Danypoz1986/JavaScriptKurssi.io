2161944fc8c4de9fe294b1e24a661d3a2d217222
Daniel Pozzoli <daniel.pozzoli86@gmail.com>
2024-09-06 14:53:10 +0300

    Add WorkShop2 folder with content from workshop2

---
+++ WorkShop2/Exercise4/jskoodi4.js
@@ -0,0 +1,5 @@
+let isStudent = confirm("Are you a student?")
+if (isStudent)
+    document.write("<h1 style='color: red'>Welcome student</h1>")
+else 
+    document.write("<h1 style='color: red'>Sorry, this page is for students only</h1>")
