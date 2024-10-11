058d7e71f0e801242d18bc9b701bb7cd17197bbe
Daniel Pozzoli <daniel.pozzoli86@gmail.com>
2024-10-11 23:46:54 +0300

    Add Dom_skriptaus_projekti with index.html, script.js, style.css, and image

--- WorkShop1/1/javascript.js
+++
@@ -1,7 +0,0 @@
-alert("Hello World"); 
-alert("Another hello World");
-alert("Still one more hello World");
-window.alert("ngfhgjh")
-document.write("hgjhjhj")
-console.log("log")
-dkjkfgrkjgk  //nonsense-->vihre
--- WorkShop1/extra1/javascript.js
+++
@@ -1,4 +0,0 @@
-function dogAge(humanAge){
-    return humanAge*7;
-}
-console.log(dogAge(5))
